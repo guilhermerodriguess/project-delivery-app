fcdbc5ab518fdd4b731bbbddafae1dd92f643c3f
Guilherme <grsguiga@gmail.com>
2023-05-11 23:14:28 -0300

    Requisito 3

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import LoginPage from './pages/login/LoginPage';
+import './App.css';
 
 function App() {
   return (
--- front-end/src/pages/login/LoginPage.js
+++ front-end/src/pages/login/LoginPage.js
@@ -1,57 +1,105 @@
 import React, { useState } from 'react';
+import './LoginPage.css';
 
 function LoginPage() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [emailValue, setEmailValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+  const [error, setError] = useState('');
+
+  const MIN_PASS_LENGTH = 6;
+
+  const validateEmail = (email) => {
+    // Validação de email incompleto e formato inválido
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return email && emailRegex.test(email);
+  };
+
+  const validatePassword = (password) => password && password.length >= MIN_PASS_LENGTH;
+
+  const handleEmailChange = (e) => {
+    const newEmail = e.target.value;
+    setEmailValue(newEmail);
+    if (!validateEmail(newEmail)) {
+      setError('Email inválido');
+    } else {
+      setError('');
+    }
+  };
+
+  const handlePasswordChange = (e) => {
+    const newPassword = e.target.value;
+    setPasswordValue(newPassword);
+    if (!validatePassword(newPassword)) {
+      setError('Senha deve ter pelo menos 6 caracteres');
+    } else {
+      setError('');
+    }
+  };
 
   const handleLogin = (e) => {
     e.preventDefault();
+    // Verifica se o email e a senha são válidos antes de prosseguir com o login
+
+    if (!validateEmail(emailValue)) {
+      setError('Email inválido');
+      return;
+    }
+
+    if (!validatePassword(passwordValue)) {
+      setError('Senha deve ter pelo menos 6 caracteres');
+    }
   };
 
   return (
-    <div>
+    <div className="login-container">
       <h1>Login</h1>
       <form onSubmit={ handleLogin }>
-        <div>
-          <label htmlFor="username">
-            Username:
+        <div className="input-wrapper">
+          <label htmlFor="email">
+            Email
             <input
+              type="email"
+              id="email"
+              value={ emailValue }
+              onChange={ handleEmailChange }
               data-testid="common_login__input-email"
-              type="text"
-              id="username"
-              value={ username }
-              onChange={ (e) => setUsername(e.target.value) }
             />
           </label>
         </div>
-        <div>
+        <div className="input-wrapper">
           <label htmlFor="password">
-            Password:
+            Senha
             <input
-              data-testid="common_login__input-password"
               type="password"
               id="password"
-              value={ password }
-              onChange={ (e) => setPassword(e.target.value) }
+              value={ passwordValue }
+              onChange={ handlePasswordChange }
+              data-testid="common_login__input-password"
             />
           </label>
         </div>
         <button
-          data-testid="common_login__button-login"
           type="submit"
+          disabled={ !validateEmail(emailValue) || !validatePassword(passwordValue) }
+          data-testid="common_login__button-login"
         >
           Login
         </button>
+        <button
+          className="button-register"
+          type="button"
+          data-testid="common_login__button-register"
+        >
+          Ainda não tenho conta
+        </button>
       </form>
-      <button
-        type="button"
-        data-testid="common_login__button-register"
+      <div
+        data-testid="common_login__element-invalid-email"
+        className={ `error-message ${error ? '' : 'hidden'}` }
       >
-        Ainda não tenho conta
-      </button>
-      <div data-testid="common_login__element-invalid-email" className="error-message" />
+        {error}
+      </div>
     </div>
-
   );
 }
 
