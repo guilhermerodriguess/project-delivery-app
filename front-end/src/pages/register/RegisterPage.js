0c47547f63a18c050e7ad7159c671bcfd9cb8f38
Guilherme <grsguiga@gmail.com>
2023-05-12 21:21:28 -0300

    Requisito 8

--- front-end/src/pages/register/RegisterPage.js
+++ front-end/src/pages/register/RegisterPage.js
@@ -3,22 +3,49 @@ import { Link, useHistory } from 'react-router-dom';
 import './RegisterPage.css';
 
 function RegisterPage() {
+  const [nameValue, setNameValue] = useState('');
   const [emailValue, setEmailValue] = useState('');
   const [passwordValue, setPasswordValue] = useState('');
-  const [nameValue, setNameValue] = useState('');
+  const [error, setError] = useState('');
 
   const history = useHistory(); // Obtém o objeto de histórico
 
+  const MIN_NAME_LENGTH = 12;
+  const MIN_PASS_LENGTH = 6;
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return email && emailRegex.test(email);
+  };
+
   const handleEmailChange = (e) => {
-    setEmailValue(e.target.value);
+    const newEmail = e.target.value;
+    setEmailValue(newEmail);
+    if (!validateEmail(newEmail)) {
+      setError('Email inválido');
+    } else {
+      setError('');
+    }
   };
 
   const handlePasswordChange = (e) => {
-    setPasswordValue(e.target.value);
+    const newPassword = e.target.value;
+    setPasswordValue(newPassword);
+    if (newPassword.length < MIN_PASS_LENGTH) {
+      setError('Senha deve ter pelo menos 6 caracteres');
+    } else {
+      setError('');
+    }
   };
 
   const handleNameChange = (e) => {
-    setNameValue(e.target.value);
+    const newName = e.target.value;
+    setNameValue(newName);
+    if (newName.length < MIN_NAME_LENGTH) {
+      setError('Nome completo deve ter pelo menos 12 caracteres');
+    } else {
+      setError('');
+    }
   };
 
   const handleRegister = (e) => {
@@ -28,6 +55,12 @@ function RegisterPage() {
     // Após o registro bem-sucedido, redireciona para a página de produtos do cliente
     history.push('/customer/products');
   };
+
+  const isFormValid = () => (
+    nameValue.length >= MIN_NAME_LENGTH
+      && validateEmail(emailValue)
+      && passwordValue.length >= MIN_PASS_LENGTH
+  );
 
   return (
     <div className="register-container">
@@ -73,14 +106,17 @@ function RegisterPage() {
           type="submit"
           className="button-register"
           data-testid="common_register__button-register"
+          disabled={ !isFormValid() }
         >
           Registrar
         </button>
       </form>
       <div
         data-testid="common_register__element-invalid_register"
-        className="hidden"
-      />
+        className={ `error-message ${error ? '' : 'hidden'}` }
+      >
+        { error }
+      </div>
       <div>
         Já possui uma conta?
         {' '}
