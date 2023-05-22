e597e7cfbdaa2e30e9b582e607f0ca914c87991e
Guilherme <grsguiga@gmail.com>
2023-05-22 18:29:39 -0300

    Requisitos 30, 31, 32, 33, 34 e 35

--- front-end/src/pages/seller/orders/SellerOrdersPage.jsx
+++ front-end/src/pages/seller/orders/SellerOrdersPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
+import Navbar from '../../../components/Navbar/Navbar';
 
 function SellerOrdersPage() {
   const [orders, setOrders] = useState([]);
@@ -21,40 +22,43 @@ function SellerOrdersPage() {
   }, []);
 
   return (
-    <div>
-      <h1>Pedidos do Vendedor</h1>
-      {orders.map((order) => (
-        <Link to={ `/seller/orders/${order.id}` } key={ order.id }>
-          <div className="order-card">
-            <h3 data-testid={ `seller_orders__element-order-id-${order.id}` }>
-              ID do Pedido:
-              {' '}
-              {order.id}
-            </h3>
-            <h4 data-testid={ `seller_orders__element-delivery-status-${order.id}` }>
-              Status de Entrega:
-              {' '}
-              {order.status}
-            </h4>
-            <p data-testid={ `seller_orders__element-order-date-${order.id}` }>
-              Data do Pedido:
-              {' '}
-              {moment(order.saleDate).format('DD/MM/YYYY')}
-            </p>
-            <p data-testid={ `seller_orders__element-card-price-${order.id}` }>
-              Preço do Pedido: R$
-              {' '}
-              {order.totalPrice}
-            </p>
-            <p data-testid={ `seller_orders__element-card-address-${order.id}` }>
-              Endereço de Entrega:
-              {' '}
-              {order.deliveryAddress}
-            </p>
-          </div>
-        </Link>
-      ))}
-    </div>
+    <>
+      <Navbar />
+      <div>
+        <h1>Pedidos do Vendedor</h1>
+        {orders.map((order) => (
+          <Link to={ `/seller/orders/${order.id}` } key={ order.id }>
+            <div className="order-card">
+              <h3 data-testid={ `seller_orders__element-order-id-${order.id}` }>
+                ID do Pedido:
+                {' '}
+                {order.id}
+              </h3>
+              <h4 data-testid={ `seller_orders__element-delivery-status-${order.id}` }>
+                Status de Entrega:
+                {' '}
+                {order.status}
+              </h4>
+              <p data-testid={ `seller_orders__element-order-date-${order.id}` }>
+                Data do Pedido:
+                {' '}
+                {moment(order.saleDate).format('DD/MM/YYYY')}
+              </p>
+              <p data-testid={ `seller_orders__element-card-price-${order.id}` }>
+                Preço do Pedido: R$
+                {' '}
+                {order.totalPrice}
+              </p>
+              <p data-testid={ `seller_orders__element-card-address-${order.id}` }>
+                Endereço de Entrega:
+                {' '}
+                {order.deliveryAddress}
+              </p>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </>
   );
 }
 
