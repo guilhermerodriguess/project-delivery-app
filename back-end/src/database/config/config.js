2a84ff0229a9e0c09d59d75cdf3ec6dc416c7304
Guilherme <grsguiga@gmail.com>
2023-06-16 23:24:15 -0300

    Initial commit

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -11,12 +11,12 @@ const suffix = {
 };
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
+  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 's-cdbr-east-06.cleardb.net',
   port: process.env.MYSQL_PORT || '3306',
   database: 
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
+    `${process.env.MYSQL_DB_NAME || 'heroku_199c82e4d4a79e1'}${suffix[environment] || suffix.test}`,
+  username: process.env.MYSQL_USER || 'b7fe5ca8e345e5',
+  password: process.env.MYSQL_PASSWORD || '063657c4',
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
@@ -35,3 +35,5 @@ module.exports = {
     ...options,
   },
 };
+
+mysql://b7fe5ca8e345e5:063657c4@us-cdbr-east-06.cleardb.net/heroku_199c82e4d4a79e1?reconnect=true
